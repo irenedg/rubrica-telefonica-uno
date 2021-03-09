76cc38f341f36adbcc2e3b5106e00815286d72ac
AulicinoADiego <aulicinodiego@gmail.com>
2021-03-09 10:13:47 +0100

    Creazione Component e Service Client

--- rubrica-telefonica-uno-client/src/app/app.module.ts
+++ rubrica-telefonica-uno-client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ErrorMessagePageComponent } from './error-message-page/error-message-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { CountPageComponent } from './count-page/count-page.component';
+import { TabellaComponent } from './tabella/tabella.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ErrorMessagePageComponent,
+    MainPageComponent,
+    SearchPageComponent,
+    CountPageComponent,
+    TabellaComponent
   ],
   imports: [
     BrowserModule,
---
+++ rubrica-telefonica-uno-client/src/app/tabella/tabella.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabella',
+  templateUrl: './tabella.component.html',
+  styleUrls: ['./tabella.component.css']
+})
+export class TabellaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
