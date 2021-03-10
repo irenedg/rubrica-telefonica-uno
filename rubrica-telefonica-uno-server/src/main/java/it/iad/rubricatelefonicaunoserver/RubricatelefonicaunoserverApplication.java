cecaae7c60f70258364cb9f79bd9220a39313d88
irenedg <79708656+irenedg@users.noreply.github.com>
2021-03-10 11:02:03 +0100

    Risolto ticket #10

---
+++ rubrica-telefonica-uno-server/src/main/java/it/iad/rubricatelefonicaunoserver/RubricatelefonicaunoserverApplication.java
@@ -0,0 +1,13 @@
+package it.iad.rubricatelefonicaunoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricatelefonicaunoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricatelefonicaunoserverApplication.class, args);
+	}
+
+}
---
+++ rubrica-telefonica-uno-server/src/test/java/it/iad/rubricatelefonicaunoserver/RubricatelefonicaunoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.rubricatelefonicaunoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricatelefonicaunoserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
