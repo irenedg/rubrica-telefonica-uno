13f9cc3ddbcd3c4a7fb89feaa2ca9c66ea312c52
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-03-10 17:14:53 +0100

    Added jpa repository enable annotation for issue #3

--- rubrica-telefonica-uno-server/src/main/java/it/iad/rubricatelefonicaunoserver/RubricatelefonicaunoserverApplication.java
+++ rubrica-telefonica-uno-server/src/main/java/it/iad/rubricatelefonicaunoserver/RubricatelefonicaunoserverApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "it.iad.rubricatelefonicaunoserver.repository")
 public class RubricatelefonicaunoserverApplication {
 
 	public static void main(String[] args) {
